738b7473fe54861735b9a1ae04dd61ec8ecabf21
Daniel San <danielgdt88@hotmail.com>
2024-09-26 18:24:51 -0300

    feat: acabo a criação do section left

--- src/components/LoginHome/SectionLeft.tsx
+++ src/components/LoginHome/SectionLeft.tsx
@@ -1,18 +1,28 @@
 export default function Sectionleft() {
   return (
     <section className="bg-sectionLeft lg:h-full lg:flex lg:flex-col ">
-      <div className="lg:px-28 lg:pt-40 flex-1">
-        <h1 className="font-semibold text-white lg:text-4xl lg:leading-normal">
+      <div className="lg:px-16 lg:pt-40 lg:flex-1">
+        <h1 className="font-semibold text-white lg:text-5xl ">
           Bem vindo(a) ao Desconecte Bem!
         </h1>
-
-        <p className="text-white text-base lg:font-normal lg:pr-16">
-          Lhe acompanhando na jornada rumo a uma vida mais balanceada.
-        </p>
       </div>
 
-      <div className="lg:self-end lg:justify-end lg:w-36">
-        <img src="/assets/logoLight.png" alt="logo" />
+      <p className="text-white text-base lg:font-normal lg:px-16 lg:mt-1 lg:mr-40">
+        Lhe acompanhando na jornada rumo a uma vida mais balanceada.
+      </p>
+
+      <div className="lg:flex lg:w-full ">
+        <img
+          src="/assets/imageSectionLeft.png"
+          alt="image-section"
+          className="lg:w-3/4 lg:h-3/4 lg:self-end"
+        />
+
+        <img
+          src="/assets/logoLight.png"
+          alt="logo"
+          className=" lg:h-44 lg:self-end lg:pb-8"
+        />
       </div>
     </section>
   );
