9a41110ccdef5a8af7d7f657289b0d3a1e7b2d72
Daniel San <danielgdt88@hotmail.com>
2024-09-26 18:46:30 -0300

    style: faço o parte do estilo da section right

--- src/components/LoginHome/SectionRight.tsx
+++ src/components/LoginHome/SectionRight.tsx
@@ -1,3 +1,45 @@
 export default function SectionRight() {
-  return <div className="bg-sectionRight lg:h-full">SECTIONRight</div>;
+  return (
+    <section className="bg-sectionRight lg:h-full lg:flex lg:flex-col lg:items-center lg:justify-center">
+      <div className="bg-white lg:w-96 lg:h-96 lg:rounded-3xl">
+        <div className="lg:flex lg:items-center">
+          <img src="/assets/logoDark.png" alt="logo-entrar" width="138px" />
+
+          <div className="lg:flex lg:flex-col">
+            <h1>Entrar</h1>
+            <p>
+              Não tem uma conta?{" "}
+              <span className="text-cadastrar cursor-pointer">
+                Cadastrar-se.
+              </span>
+            </p>
+          </div>
+        </div>
+
+        <form className="lg:flex lg:flex-col lg:pt-10 lg:px-9 lg:gap-y-5">
+          <label htmlFor="email"></label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="E-mail"
+            className="outline-none"
+          />
+
+          <label htmlFor="password"></label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Senha"
+            className="outline-none"
+          />
+
+          <a href="#" className="underline lg:self-center lg:pt-4">
+            Esqueci minha senha
+          </a>
+        </form>
+      </div>
+    </section>
+  );
 }
