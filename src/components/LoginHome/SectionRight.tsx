7f77020f9c43b3b6c0d5bc797495e766b2e5b409
Daniel San <danielgdt88@hotmail.com>
2024-09-27 10:45:19 -0300

    feat: eu crio o bloco branco para inputs e ancora

--- src/components/LoginHome/SectionRight.tsx
+++ src/components/LoginHome/SectionRight.tsx
@@ -1,12 +1,12 @@
 export default function SectionRight() {
   return (
     <section className="bg-sectionRight lg:h-full lg:flex lg:flex-col lg:items-center lg:justify-center">
-      <div className="bg-white lg:w-96 lg:h-96 lg:rounded-3xl">
+      <div className="bg-white lg:w-96 lg:h-96 lg:rounded-3xl lg:flex lg:flex-col">
         <div className="lg:flex lg:items-center">
           <img src="/assets/logoDark.png" alt="logo-entrar" width="138px" />
 
           <div className="lg:flex lg:flex-col">
-            <h1>Entrar</h1>
+            <h1 className="lg:text-5xl lg:font-semibold">Entrar</h1>
             <p>
               Não tem uma conta?{" "}
               <span className="text-cadastrar cursor-pointer">
@@ -16,29 +16,29 @@ export default function SectionRight() {
           </div>
         </div>
 
-        <form className="lg:flex lg:flex-col lg:pt-10 lg:px-9 lg:gap-y-5">
-          <label htmlFor="email"></label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            placeholder="E-mail"
-            className="outline-none"
-          />
-
-          <label htmlFor="password"></label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="Senha"
-            className="outline-none"
-          />
-
-          <a href="#" className="underline lg:self-center lg:pt-4">
-            Esqueci minha senha
-          </a>
+        <form className="lg:flex lg:flex-col lg:pt-10 lg:px-9 lg:gap-y-16">
+          <div className="relative barra-horizontal">
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="E-mail"
+              className="outline-none placeholderMax"
+            />
+          </div>
+          <div className="relative barra-horizontal">
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Senha"
+              className="outline-none placeholderMax"
+            />
+          </div>
         </form>
+        <a href="#" className="underline lg:self-center lg:pt-10">
+          Esqueci minha senha
+        </a>
       </div>
     </section>
   );
