ebaa54fe42e01766c4dada32f888fbecd71883ba
Daniel San <danielgdt88@hotmail.com>
2024-10-01 20:06:36 -0300

    wix: começo criação do responsivo mobile

--- src/components/LoginHome/SectionRight.tsx
+++ src/components/LoginHome/SectionRight.tsx
@@ -1,12 +1,19 @@
 export default function SectionRight() {
   return (
-    <section className="h-screen bg-sectionRight lg:h-full lg:flex lg:flex-col lg:items-center lg:justify-center">
-      <div className="bg-sectionRight lg:bg-white lg:w-96 lg:h-96 lg:rounded-3xl lg:flex lg:flex-col">
+    <section className="h-screen bg-sectionRight flex flex-col justify-center items-center lg:h-full ">
+      <div className="p-6 bg-white rounded-3xl flex flex-col lg:w-96 lg:h-96">
         <div className="lg:flex lg:items-center">
-          <img src="/assets/logoDark.png" alt="logo-entrar" width="138px" />
+          <img
+            src="/assets/logoDark.png"
+            alt="logo-entrar"
+            width="138px"
+            className="hidden md:block"
+          />
 
-          <div className="lg:flex lg:flex-col">
-            <h1 className="lg:text-5xl lg:font-semibold">Entrar</h1>
+          <div className="pb-7 lg:pb-0 lg:flex lg:flex-col">
+            <h1 className="text-4xl text-center font-semibold lg:text-5xl lg:text-start">
+              Entrar
+            </h1>
             <p>
               Não tem uma conta?{" "}
               <span className="text-cadastrar cursor-pointer">
@@ -26,7 +33,7 @@ export default function SectionRight() {
               name="email"
               id="email"
               placeholder="E-mail"
-              className="outline-none placeholderMax w-full"
+              className="outline-none md:placeholderMax w-full"
             />
           </div>
           <div className="relative barra-horizontal ">
@@ -35,19 +42,22 @@ export default function SectionRight() {
               name="password"
               id="password"
               placeholder="Senha"
-              className="outline-none placeholderMax w-full"
+              className="outline-none md:placeholderMax w-full"
             />
           </div>
         </form>
-        <a href="#" className="underline lg:self-center lg:pt-10">
+        <a
+          href=""
+          className="self-center underline pt-7 lg:self-center lg:pt-10"
+        >
           Esqueci minha senha
         </a>
       </div>
-      <div className="lg:pt-14">
+      <div className="pt-7 lg:pt-14">
         <button
           type="submit"
           id="form-email"
-          className="text-white font-semibold bg-botaoLogin lg:text-4xl lg:py-4 lg:px-20 lg:rounded-l-full lg:rounded-br-full lg:boxShadowButton"
+          className="text-white font-semibold bg-botaoLogin text-3xl px-12 py-2 lg:text-4xl lg:py-4 lg:px-20 rounded-l-full rounded-br-full boxShadowButton"
         >
           Entrar
         </button>
