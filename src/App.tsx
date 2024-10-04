68e36d2e2fb6f5814f3170a2c33533e0592e15f9
Daniel San <danielgdt88@hotmail.com>
2024-10-04 17:35:08 -0300

    feat: crio componente EsqueciSenha

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomeLogin from "./components/HomeLogin";
 import HomeCadastro from "./components/HomeCadastro";
+import EsqueciSenha from "./components/EsqueciSenha";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<HomeLogin />} />
         <Route path="cadastro" element={<HomeCadastro />} />
+        <Route path="esqueci-senha" element={<EsqueciSenha />} />
       </Routes>
     </BrowserRouter>
   );
