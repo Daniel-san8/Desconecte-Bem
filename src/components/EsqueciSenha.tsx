42c814a58d4b3a4eb98fa17e3f5a308f96ad2835
Daniel San <danielgdt88@hotmail.com>
2024-10-04 17:37:20 -0300

    feat: crio atualização de title para componente EsqueciSenha

--- src/components/EsqueciSenha.tsx
+++ src/components/EsqueciSenha.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function EsqueciSenha() {
   React.useEffect(() => {
     document.title = "Esqueci a Senha | Desconecte Bem";
-  });
+  }, []);
 
   return <h1>Esqueci a senha</h1>;
 }
