c7269fd9ec5a24361f17ef87cc29c42c00a7409f
Daniel San <danielgdt88@hotmail.com>
2024-10-07 18:25:40 -0300

    feat: step four mobile completa

--- src/components/EsqueciSenha/StepFour.tsx
+++ src/components/EsqueciSenha/StepFour.tsx
@@ -2,28 +2,38 @@ import { useNavigate } from "react-router-dom";
 import { useGlobal } from "../../globalStates/globalUse";
 import BoxTitle from "../LoginHome/BoxTitle";
 import ButtonLogin from "../LoginHome/ButtonLogin";
-import WhiteBox from "../LoginHome/WhiteBox";
 
 export default function StepFour() {
   const navigate = useNavigate();
+
   const global = useGlobal();
+
   function backLogin() {
     global.nextStep();
     navigate("/");
   }
+
   return (
     <>
       <div className="bg-retangulo w-full h-24 mt-8 md:hidden"></div>
-      <WhiteBox espacamento="mt-28">
+
+      <div
+        className="
+      p-5 mt-28 bg-white rounded-3xl w-72 h-52 flex z-30 flex-col md:w-80 md:h-96 md:p-0 md:mt-6 lg:w-96"
+      >
+        <div className="flex items-center">
+          <h1 className="text-2xl text-center font-semibold my-4 md:text-4xl md:text-start">
+            Senha alterada com sucesso!
+          </h1>
+          <div>
+            <img src="/assets/confirmarSenha.png" alt="senha confirmada" />
+          </div>
+        </div>
         <BoxTitle
-          titulo="Senha alterada com sucesso!"
+          className="text-sm text-center mb-2"
           pergunta="Conseguimos vincular a nova senha à sua conta. Agora, você pode entrar com ela normalmente."
         />
-        <div className="self-end ">
-          <img src="/assets/confirmarSenha.png" alt="senha confirmada" />
-        </div>
-      </WhiteBox>
-
+      </div>
       <ButtonLogin
         functionButton={backLogin}
         texto="Entrar na conta"
